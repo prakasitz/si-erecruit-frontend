b3a3a02b3c63866ae85dfc9506ca19d39621c988
prakasit.chu <prakasit.chu@mahidol.ac.th>
2023-03-10 15:56:13 +0700

    add vuetify3 for nuxt3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-    modules: [],
     typescript: {
         strict: true,
         typeCheck: true,
+    },
+
+    css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.min.css'], // vuetify ships precompiled css, no need to import sass
+    build: {
+        transpile: ['vuetify'],
+    },
+    vite: {
+        define: {
+            'process.env.DEBUG': false,
+        },
     },
 
     // auto import components
