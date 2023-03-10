5fbae18014c47f12073a140cd1b7bed5a1b76578
prakasit.chu <prakasit.chu@mahidol.ac.th>
2023-03-10 14:56:10 +0700

    init ts for nuxtjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: [],
+    typescript: {
+        strict: true,
+        typeCheck: true,
+    },
 
+    // auto import components
+    components: true,
+    app: {
+        // global transition
+        pageTransition: { name: 'page', mode: 'out-in' },
+        layoutTransition: { name: 'layout', mode: 'out-in' },
+    },
 })
