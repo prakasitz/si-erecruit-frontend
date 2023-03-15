84e05e8a64eb3fae2ec78cd8c120ecee541defed
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-03-15 09:04:58 +0700

    update Topicbar max-height

--- components/TopicBar.vue
+++ components/TopicBar.vue
@@ -1,8 +1,8 @@
 <template>
-    <v-sheet elevation="5" style="margin-bottom: 30px">
+    <v-sheet elevation="2" style="margin-bottom: 30px">
         <v-row no-gutters>
             <v-col cols="12">
-                <v-sheet class="ma-1 pa-5">
+                <v-sheet class="ma-1 pa-5" max-height="120px">
                     <span
                         :style="['font-family: \'Krub-Bold\' !important']"
                         class="text-h5 font-weight-bold text-main-color"
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
     <NuxtLayout>
         <div>
+            <TopicBar />
+            <v-card class="mx-auto" width="90%">
+                <v-card-title :style="{ 'font-weight': 'bold', 'font-size': '16px !important' }"
+                    >ยินดีต้อนรับคุณ นาย ประกาศิต ช่วยรักษา</v-card-title
+                >
+            </v-card>
+            <br />
             <v-card class="mx-auto" width="400" prepend-icon="mdi-home">
                 <template v-slot:title>Hello from Vuetify!</template>
                 <v-card-text>When you see this inside a card, it worked!</v-card-text>
@@ -8,3 +15,5 @@
         </div>
     </NuxtLayout>
 </template>
+
+<script setup></script>
