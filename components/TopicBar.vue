baf835ffb4a557a0ce038927c71bc4fce32f1c73
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-03-15 08:49:51 +0700

    TopicBar for Main Erecruit

---
+++ components/TopicBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-sheet elevation="5" style="margin-bottom: 30px">
+        <v-row no-gutters>
+            <v-col cols="12">
+                <v-sheet class="ma-1 pa-5">
+                    <span
+                        :style="['font-family: \'Krub-Bold\' !important']"
+                        class="text-h5 font-weight-bold text-main-color"
+                    >
+                        เมนูทั่วไป
+                    </span>
+                    <v-breadcrumbs divider=">" :items="['Foo', 'Bar', 'Fizz']"></v-breadcrumbs>
+                </v-sheet>
+            </v-col>
+        </v-row>
+    </v-sheet>
+</template>
