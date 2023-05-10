89a9a3c5b7c87c5e78748fe4025165a135b8a196
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-05-10 13:33:11 +0700

    form personal

--- components/candidate/Personal.vue
+++ components/candidate/Personal.vue
@@ -7,18 +7,7 @@
                     <v-col cols="12">
                         <v-row>
                             <v-col cols="3">
-                                <p>
-                                    ยศ/ราชทินนาม
-                                    <v-tooltip text="ที่เปิดรับบรรจุ" location="bottom">
-                                        <template v-slot:activator="{ props }">
-                                            <v-icon
-                                                v-bind="props"
-                                                size="x-small"
-                                                icon="mdi-information-variant-circle-outline"
-                                            ></v-icon>
-                                        </template>
-                                    </v-tooltip>
-                                </p>
+                                <p>ยศ/ราชทินนาม</p>
                             </v-col>
                             <v-col cols="6">
                                 <v-select
@@ -26,8 +15,7 @@
                                     :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
                                     variant="outlined"
                                     density="compact"
-                                    :persistent-hint="true"
-                                    hint="123123"
+                                    hide-details
                                 >
                                 </v-select>
                             </v-col>
@@ -36,18 +24,7 @@
                     <v-col cols="12">
                         <v-row>
                             <v-col cols="3">
-                                <p>
-                                    ตำแหน่งทางวิชาชีพ
-                                    <v-tooltip text="ที่เปิดรับบรรจุ" location="bottom">
-                                        <template v-slot:activator="{ props }">
-                                            <v-icon
-                                                v-bind="props"
-                                                size="x-small"
-                                                icon="mdi-information-variant-circle-outline"
-                                            ></v-icon>
-                                        </template>
-                                    </v-tooltip>
-                                </p>
+                                <p>ตำแหน่งทางวิชาชีพ</p>
                             </v-col>
                             <v-col cols="6">
                                 <v-select
@@ -55,8 +32,7 @@
                                     :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
                                     variant="outlined"
                                     density="compact"
-                                    :persistent-hint="true"
-                                    hint="123123"
+                                    hide-details
                                 >
                                 </v-select>
                             </v-col>
@@ -65,18 +41,7 @@
                     <v-col cols="12">
                         <v-row>
                             <v-col cols="3">
-                                <p>
-                                    ตำแหน่งทางวิชาการ
-                                    <v-tooltip text="ที่เปิดรับบรรจุ" location="bottom">
-                                        <template v-slot:activator="{ props }">
-                                            <v-icon
-                                                v-bind="props"
-                                                size="x-small"
-                                                icon="mdi-information-variant-circle-outline"
-                                            ></v-icon>
-                                        </template>
-                                    </v-tooltip>
-                                </p>
+                                <p>ตำแหน่งทางวิชาการ</p>
                             </v-col>
                             <v-col cols="6">
                                 <v-select
@@ -84,8 +49,7 @@
                                     :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
                                     variant="outlined"
                                     density="compact"
-                                    :persistent-hint="true"
-                                    hint="123123"
+                                    hide-details
                                 >
                                 </v-select>
                             </v-col>
@@ -94,18 +58,7 @@
                     <v-col cols="12">
                         <v-row>
                             <v-col cols="3">
-                                <p>
-                                    ยศทางทหาร
-                                    <v-tooltip text="ที่เปิดรับบรรจุ" location="bottom">
-                                        <template v-slot:activator="{ props }">
-                                            <v-icon
-                                                v-bind="props"
-                                                size="x-small"
-                                                icon="mdi-information-variant-circle-outline"
-                                            ></v-icon>
-                                        </template>
-                                    </v-tooltip>
-                                </p>
+                                <p>ยศทางทหาร</p>
                             </v-col>
                             <v-col cols="6">
                                 <v-select
@@ -113,23 +66,35 @@
                                     :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
                                     variant="outlined"
                                     density="compact"
-                                    :persistent-hint="true"
-                                    hint="123123"
+                                    hide-details
                                 >
                                 </v-select>
                             </v-col>
                         </v-row>
                     </v-col>
 
+                    <v-col cols="12">
+                        <v-divider></v-divider>
+                    </v-col>
+
                     <!-- ชื่อ-นามสกุล -->
                     <v-col cols="12">
-                        <v-text-field density="compact" variant="outlined" value="1 4098 00357 95 0" hide-details>
-                            <template v-slot:prepend>
-                                <p :style="{ 'font-size': '1.12rem !important' }">
-                                    <b>คำนำหน้า (ภาษาไทย)</b>
-                                </p>
-                            </template>
-                        </v-text-field>
+                        <v-row>
+                            <v-col cols="3">
+                                <p>ยศทางทหาร</p>
+                            </v-col>
+                            <v-col cols="6">
+                                <v-select
+                                    label="กรุณาเลือก"
+                                    :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+                                    variant="outlined"
+                                    density="compact"
+                                    :persistent-hint="true"
+                                    hint="123123"
+                                >
+                                </v-select>
+                            </v-col>
+                        </v-row>
                     </v-col>
                     <v-col cols="6">
                         <v-text-field density="compact" variant="outlined" value="1 4098 00357 95 0" hide-details>
