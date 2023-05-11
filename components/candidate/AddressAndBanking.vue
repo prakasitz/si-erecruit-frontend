6628353174e2fbd3072b0af9d85754be42add7b9
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-05-11 10:38:31 +0700

    form addressAndBanking

--- components/candidate/AddressAndBanking.vue
+++ components/candidate/AddressAndBanking.vue
@@ -1,5 +1,106 @@
 <template>
     <CandidateBaseCard :title="'ข้อมูลที่อยู่/บัญชีธนาคาร'">
-        <template #card-body> asdasdad </template>
+        <template #card-body>
+            <v-container class="text-body-1">
+                <v-row>
+                    <!-- คำนำหน้า พิเศษ -->
+
+                    <v-col cols="3">
+                        <b>ที่อยู่ตามทะเบียนบ้าน <span class="text-red">*</span></b>
+                    </v-col>
+                    <v-col>
+                        <FormsHouseRegistration />
+                    </v-col>
+                </v-row>
+            </v-container>
+
+            <v-divider class="my-2"></v-divider>
+
+            <v-container class="text-body-1">
+                <v-row>
+                    <v-col cols="3">
+                        <b>ที่อยู่ปัจจุบัน <span class="text-red">*</span></b>
+                    </v-col>
+                    <v-col>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-checkbox
+                                    label="ใช้ที่อยู่เดียวกันกับ ที่ตามทะเบียนบ้าน"
+                                    hide-details
+                                    density="compact"
+                                    v-model="currIsHomeland"
+                                >
+                                </v-checkbox>
+                            </v-col>
+                            <v-col class="pt-0">
+                                <v-expand-transition>
+                                    <div v-if="!currIsHomeland">
+                                        <FormsHouseRegistration />
+                                    </div>
+                                </v-expand-transition>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-row>
+            </v-container>
+
+            <v-divider class="my-2"></v-divider>
+
+            <v-container class="text-body-1">
+                <v-row>
+                    <v-col cols="3">
+                        <b>ที่อยู่ฉุกเฉิน <span class="text-red">*</span></b>
+                        <p class="text-caption">(ที่สามารถติดต่อได้)</p>
+                    </v-col>
+                    <v-col>
+                        <v-row>
+                            <v-col cols="12" class="pb-0">
+                                <v-checkbox
+                                    label="ใช้ที่อยู่เดียวกันกับ ที่ตามทะเบียนบ้าน"
+                                    hide-details
+                                    density="compact"
+                                    v-model="emerIsHomeland"
+                                >
+                                </v-checkbox>
+                            </v-col>
+                            <v-col cols="12" v-if="!currIsHomeland">
+                                <v-checkbox
+                                    label="ใช้ที่อยู่เดียวกันกับ ที่อยู่ปัจจุบัน"
+                                    hide-details
+                                    density="compact"
+                                    v-model="emerIsCurrent"
+                                >
+                                </v-checkbox>
+                            </v-col>
+                            <v-col>
+                                <v-expand-transition>
+                                    <div v-if="!emerIsCurrent && !emerIsHomeland">
+                                        <FormsHouseRegistration />
+                                    </div>
+                                </v-expand-transition>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </template>
     </CandidateBaseCard>
 </template>
+
+<script setup lang="ts">
+const currIsHomeland = ref(false)
+const emerIsHomeland = ref(false)
+const emerIsCurrent = ref(false)
+
+watch(emerIsHomeland, (newValue) => {
+    if (newValue === true) {
+        emerIsCurrent.value = false
+    }
+})
+
+watch(emerIsCurrent, (newValue) => {
+    if (newValue === true) {
+        emerIsHomeland.value = false
+    }
+})
+</script>
---
+++ components/forms/HouseRegistration.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-card variant="outlined">
+        <v-card-text class="text-body-1">
+            <v-row>
+                <v-col md="6" sm="12" cols="12">
+                    <v-text-field label="บ้านเลขที่" hide-details density="compact" variant="outlined"> </v-text-field>
+                </v-col>
+                <v-col md="6" sm="12" cols="12">
+                    <v-text-field label="หมู่" hide-details density="compact" variant="outlined"> </v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col md="6" sm="12" cols="12">
+                    <v-text-field label="หมู่บ้าน" hide-details density="compact" variant="outlined"> </v-text-field>
+                </v-col>
+                <v-col md="6" sm="12" cols="12">
+                    <v-text-field label="ซอย" hide-details density="compact" variant="outlined"> </v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col md="6" sm="12" cols="12">
+                    <v-text-field label="ถนน" hide-details density="compact" variant="outlined"> </v-text-field>
+                </v-col>
+                <v-col md="6" sm="12" cols="12">
+                    <v-text-field label="ตำบล/แขวง" hide-details density="compact" variant="outlined"> </v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col md="6" sm="12" cols="12">
+                    <v-text-field label="อำเภอ/เขต" hide-details density="compact" variant="outlined"> </v-text-field>
+                </v-col>
+                <v-col md="6" sm="12" cols="12">
+                    <v-text-field label="จังหวัด" hide-details density="compact" variant="outlined"> </v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col md="6" sm="12" cols="12">
+                    <v-text-field label="รหัสไปรษณีย์" hide-details density="compact" variant="outlined">
+                    </v-text-field>
+                </v-col>
+                <v-col md="6" sm="12" cols="12">
+                    <v-text-field label="ประเทศ" hide-details density="compact" variant="outlined"> </v-text-field>
+                </v-col>
+            </v-row>
+        </v-card-text>
+    </v-card>
+</template>
