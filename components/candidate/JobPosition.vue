ded2a99c019fc12138874467ff067b32c85ec6f5
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-05-12 11:30:46 +0700

    add form jobPostion

--- components/candidate/JobPosition.vue
+++ components/candidate/JobPosition.vue
@@ -1,19 +1,38 @@
 <template>
     <CandidateBaseCard :title="'ข้อมูลตำแหน่งงาน'">
         <template #card-body>
+            <v-alert
+                class="text-subtitle-2"
+                border="start"
+                type="info"
+                variant="outlined"
+                icon="mdi-information-outline"
+                :text="jobPositionDescription"
+            ></v-alert>
             <v-container class="text-body-1 my-6">
                 <v-row>
-                    <v-col cols="3">
+                    <v-col cols="2">
                         <p>
                             <b>ตำแหน่ง <span class="text-red">*</span></b>
                         </p>
                         <p class="text-caption">ที่เปิดรับบรรจุ</p>
                     </v-col>
-                    <v-col cols="8">
+                    <v-col cols>
                         <v-expand-transition>
                             <div v-if="true">
-                                <FormsJobsChoice />
-                                <FormsJobsChoice />
+                                <v-item-group selected-class="card-item-selected">
+                                    <v-row>
+                                        <v-col cols="6" v-for="i in 4">
+                                            <v-item v-slot="{ isSelected, selectedClass, toggle }">
+                                                <FormsJobsChoice
+                                                    :is-selected="isSelected"
+                                                    :class="[selectedClass]"
+                                                    @click="toggle"
+                                                />
+                                            </v-item>
+                                        </v-col>
+                                    </v-row>
+                                </v-item-group>
                             </div>
                         </v-expand-transition>
                     </v-col>
@@ -22,3 +41,14 @@
         </template>
     </CandidateBaseCard>
 </template>
+
+<style scoped>
+.card-item-selected {
+    border-left-color: rgb(var(--v-theme-main-color));
+    border-left-width: 8px;
+}
+</style>
+
+<script setup lang="ts">
+const jobPositionDescription = 'กรุณาเลือกตำแหน่งที่ท่านบรรจุ'
+</script>
--- components/forms/JobsChoice.vue
+++ components/forms/JobsChoice.vue
@@ -1,45 +1,86 @@
 <template>
-    <v-card class="mb-5" variant="outlined">
-        <v-card-text class="text-body-1">
-            <v-container>
-                <v-row>
-                    <v-col cols="12">
-                        <v-text-field hide-details density="compact" variant="outlined" readonly value="พนักงานมหาลัย"
-                            ><template #prepend>
-                                <div><b>ประเภทการจ้าง</b></div>
-                            </template>
-                        </v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                        <v-text-field hide-details density="compact" variant="outlined" readonly value="ฝ่ายสารสนเทศ"
-                            ><template #prepend>
-                                <div><b>สังกัด</b></div>
-                            </template>
-                        </v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                        <v-text-field
-                            hide-details
-                            density="compact"
-                            variant="outlined"
-                            readonly
-                            value="งานวิเคราะห์และพัฒนาโปรแกรม"
-                            ><template #prepend>
-                                <div><b>งาน</b></div>
-                            </template>
-                        </v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                        <v-text-field hide-details density="compact" variant="outlined" readonly value="8:30 - 16.30 น."
-                            ><template #prepend>
-                                <div><b>เวลาปฏิบัติงาน</b></div>
-                            </template>
-                        </v-text-field>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-card-text>
+    <v-card class="mb-5" variant="outlined" readonly>
+        <v-row no-gutters>
+            <v-col cols="1" align-self="start" class="pt-4 mr-2">
+                <v-radio readonly :ripple="false" hide-details :model-value="isSelected"></v-radio>
+            </v-col>
+            <v-divider class="pa-1" vertical></v-divider>
+            <v-col cols>
+                <v-card-text class="text-body-1 v-col v-col" readonly>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field
+                                hide-details
+                                density="compact"
+                                variant="underlined"
+                                readonly
+                                value="พนักงานมหาลัย"
+                                ><template #prepend>
+                                    <div><b>ประเภทการจ้าง</b></div>
+                                </template>
+                            </v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field
+                                hide-details
+                                density="compact"
+                                variant="underlined"
+                                readonly
+                                value="ฝ่ายสารสนเทศ"
+                                ><template #prepend>
+                                    <div><b>สังกัด</b></div>
+                                </template>
+                            </v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field
+                                hide-details
+                                density="compact"
+                                variant="underlined"
+                                readonly
+                                value="งานวิเคราะห์และพัฒนาโปรแกรม"
+                                ><template #prepend>
+                                    <div><b>งาน</b></div>
+                                </template>
+                            </v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field
+                                hide-details
+                                density="compact"
+                                variant="underlined"
+                                readonly
+                                value="8:30 - 16.30 น."
+                                ><template #prepend>
+                                    <div><b>เวลาปฏิบัติงาน</b></div>
+                                </template>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-card-text></v-col
+            >
+        </v-row>
     </v-card>
 </template>
 
-<script setup lang="ts"></script>
+<style scoped>
+.v-input:deep() .v-field {
+    font-size: 14px;
+}
+
+.v-input:deep() input {
+    cursor: pointer;
+}
+
+.v-card {
+    transition: border-left-width 0.1s ease-in-out;
+}
+</style>
+
+<script setup lang="ts">
+interface Props {
+    isSelected: boolean
+}
+
+const props = defineProps<Props>()
+</script>
