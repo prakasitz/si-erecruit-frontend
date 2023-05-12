c61b3b3c08cbc906f10029306254cd416519fb64
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-05-12 08:44:49 +0700

    update vuetify to 3.2.4

--- components/candidate/JobPosition.vue
+++ components/candidate/JobPosition.vue
@@ -1,62 +1,24 @@
 <template>
     <CandidateBaseCard :title="'ข้อมูลตำแหน่งงาน'">
         <template #card-body>
-            <v-row>
-                <v-col cols="7">
-                    <v-text-field readonly density="compact" variant="outlined" value="1 4098 00357 95 0" hide-details>
-                        <template v-slot:prepend>
-                            <p :style="{ 'font-size': '1.12rem !important' }">
-                                <b>ตำแหน่ง <span class="text-red">*</span></b>
-                                <v-tooltip text="ที่เปิดรับบรรจุ" location="bottom">
-                                    <template v-slot:activator="{ props }">
-                                        <v-icon
-                                            v-bind="props"
-                                            size="x-small"
-                                            icon="mdi-information-variant-circle-outline"
-                                        ></v-icon>
-                                    </template>
-                                </v-tooltip>
-                            </p>
-                        </template>
-                    </v-text-field>
-                </v-col>
-                <v-col cols="12">
-                    <v-text-field density="compact" variant="outlined" value="1 4098 00357 95 0" hide-details>
-                        <template v-slot:prepend>
-                            <p :style="{ 'font-size': '1.12rem !important' }">
-                                <b>ประเภทการจ้าง</b>
-                            </p>
-                        </template>
-                    </v-text-field>
-                </v-col>
-                <v-col cols="6">
-                    <v-text-field density="compact" variant="outlined" value="1 4098 00357 95 0" hide-details>
-                        <template v-slot:prepend>
-                            <p :style="{ 'font-size': '1.12rem !important' }">
-                                <b>สังกัด</b>
-                            </p>
-                        </template>
-                    </v-text-field>
-                </v-col>
-                <v-col cols="6">
-                    <v-text-field density="compact" variant="outlined" value="1 4098 00357 95 0" hide-details>
-                        <template v-slot:prepend>
-                            <p :style="{ 'font-size': '1.12rem !important' }">
-                                <b>งาน</b>
-                            </p>
-                        </template>
-                    </v-text-field>
-                </v-col>
-                <v-col cols="6">
-                    <v-text-field density="compact" variant="outlined" value="1 4098 00357 95 0" hide-details>
-                        <template v-slot:prepend>
-                            <p :style="{ 'font-size': '1.12rem !important' }">
-                                <b>เวลาปฏิบัติงาน</b>
-                            </p>
-                        </template>
-                    </v-text-field>
-                </v-col>
-            </v-row>
+            <v-container class="text-body-1 my-6">
+                <v-row>
+                    <v-col cols="3">
+                        <p>
+                            <b>ตำแหน่ง <span class="text-red">*</span></b>
+                        </p>
+                        <p class="text-caption">ที่เปิดรับบรรจุ</p>
+                    </v-col>
+                    <v-col cols="8">
+                        <v-expand-transition>
+                            <div v-if="true">
+                                <FormsJobsChoice />
+                                <FormsJobsChoice />
+                            </div>
+                        </v-expand-transition>
+                    </v-col>
+                </v-row>
+            </v-container>
         </template>
     </CandidateBaseCard>
 </template>
---
+++ components/forms/JobsChoice.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-card class="mb-5" variant="outlined">
+        <v-card-text class="text-body-1">
+            <v-container>
+                <v-row>
+                    <v-col cols="12">
+                        <v-text-field hide-details density="compact" variant="outlined" readonly value="พนักงานมหาลัย"
+                            ><template #prepend>
+                                <div><b>ประเภทการจ้าง</b></div>
+                            </template>
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-text-field hide-details density="compact" variant="outlined" readonly value="ฝ่ายสารสนเทศ"
+                            ><template #prepend>
+                                <div><b>สังกัด</b></div>
+                            </template>
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-text-field
+                            hide-details
+                            density="compact"
+                            variant="outlined"
+                            readonly
+                            value="งานวิเคราะห์และพัฒนาโปรแกรม"
+                            ><template #prepend>
+                                <div><b>งาน</b></div>
+                            </template>
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-text-field hide-details density="compact" variant="outlined" readonly value="8:30 - 16.30 น."
+                            ><template #prepend>
+                                <div><b>เวลาปฏิบัติงาน</b></div>
+                            </template>
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup lang="ts"></script>
