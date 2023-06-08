819e2317271e5f56516ce58d0f4a04a53879a871
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-08 15:59:26 +0700

    add util function

--- utils/deepCopy.ts
+++
@@ -1,4 +0,0 @@
-export const deepCopy = (obj: object) => {
-    let newObj = JSON.parse(JSON.stringify(obj))
-    return newObj
-}
---
+++ utils/default.ts
@@ -0,0 +1,11 @@
+import { job } from "~/stores/interface/personal_information.interface";
+
+export const default_job: job = {
+    company_name: '',
+    end_date: '',
+    position_name: '',
+    reason: '',
+    salary: '',
+    still_doing: false,
+    start_date: '',
+}
