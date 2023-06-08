bcb533d1a65c52293414e9ba8faa7d74d5b99b82
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-08 16:11:17 +0700

    mapping state Tax

--- components/candidate/Tax.vue
+++ components/candidate/Tax.vue
@@ -1,51 +1,81 @@
 <template>
     <CandidateBaseCard :title="'ข้อมูลภาษี'">
         <template #card-body>
-            <v-alert border="start" type="info" variant="outlined" icon="mdi-information-outline"
-                text="ข้อมูลภาษี (ถ้ามี) กรุณาระบุค่าลดหย่อนภาษีบุตร และค่าลดหย่อนอื่น ๆ">
+            <v-alert
+                border="start"
+                type="info"
+                variant="outlined"
+                icon="mdi-information-outline"
+                text="ข้อมูลภาษี (ถ้ามี) กรุณาระบุค่าลดหย่อนภาษีบุตร และค่าลดหย่อนอื่น ๆ"
+            >
             </v-alert>
             <v-container class="text-body-1">
-                <p class="pb-6 text-h6"> ค่าลดหย่อนภาษีบุตร </p>
+                <p class="pb-6 text-h6">ค่าลดหย่อนภาษีบุตร</p>
                 <v-row>
                     <v-col cols="4">
-                        <p> จำนวนบุตร <span class="text-red-darken-1"> *</span> </p>
+                        <p>จำนวนบุตร <span class="text-red-darken-1"> *</span></p>
                     </v-col>
                     <v-col cols="6">
-                        <v-text-field v-model="_tax.num_of_child" label="จำนวนบุตร" hint="หากไม่มีใส่ 0" density="compact"
-                            variant="outlined" maxLength="1">
+                        <v-text-field
+                            v-model="tax.num_of_child"
+                            label="จำนวนบุตร"
+                            hint="หากไม่มีใส่ 0"
+                            density="compact"
+                            variant="outlined"
+                            maxLength="1"
+                        >
                             <template #append>คน</template>
                         </v-text-field>
                     </v-col>
                 </v-row>
-                <div id="had_chlids" v-if="_tax.num_of_child > 0">
+                <div id="had_chlids" v-if="tax.num_of_child > 0">
                     <v-row>
                         <v-col cols="4">
-                            <p>จำนวนบุตรที่ยังไม่เข้าโรงเรียน <span class="text-red-darken-1">*</span> </p>
+                            <p>จำนวนบุตรที่ยังไม่เข้าโรงเรียน <span class="text-red-darken-1">*</span></p>
                         </v-col>
                         <v-col cols="6">
-                            <v-text-field v-model="_tax.chlid_nonschool" label="จำนวน" hint="หากไม่มีใส่ 0"
-                                density="compact" variant="outlined" maxLength="1">
-                                <template #append>คน</template></v-text-field>
-                        </v-col>
-
-
-                    </v-row>
-                    <v-row>
-                        <v-col cols="4">
-                            <p> จำนวนบุตรที่กำลังศึกษาอยู่ <span class="text-red-darken-1">* </span></p>
-                        </v-col>
-                        <v-col cols="6">
-                            <v-text-field v-model="_tax.chlid_school" label="จำนวน" hint="หากไม่มีใส่ 0" density="compact"
-                                variant="outlined" maxLength="1"> <template #append>คน</template></v-text-field>
+                            <v-text-field
+                                v-model="tax.chlid_nonschool"
+                                label="จำนวน"
+                                hint="หากไม่มีใส่ 0"
+                                density="compact"
+                                variant="outlined"
+                                maxLength="1"
+                            >
+                                <template #append>คน</template></v-text-field
+                            >
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="4">
-                            <p> จำนวนบุตรที่จบการศึกษาแล้ว <span class="text-red-darken-1">* </span></p>
+                            <p>จำนวนบุตรที่กำลังศึกษาอยู่ <span class="text-red-darken-1">* </span></p>
                         </v-col>
                         <v-col cols="6">
-                            <v-text-field v-model="_tax.chlid_school" label="จำนวน" hint="หากไม่มีใส่ 0" density="compact"
-                                variant="outlined" maxLength="1">
+                            <v-text-field
+                                v-model="tax.chlid_school"
+                                label="จำนวน"
+                                hint="หากไม่มีใส่ 0"
+                                density="compact"
+                                variant="outlined"
+                                maxLength="1"
+                            >
+                                <template #append>คน</template></v-text-field
+                            >
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="4">
+                            <p>จำนวนบุตรที่จบการศึกษาแล้ว <span class="text-red-darken-1">* </span></p>
+                        </v-col>
+                        <v-col cols="6">
+                            <v-text-field
+                                v-model="tax.chlid_school"
+                                label="จำนวน"
+                                hint="หากไม่มีใส่ 0"
+                                density="compact"
+                                variant="outlined"
+                                maxLength="1"
+                            >
                                 <template #append>คน</template>
                             </v-text-field>
                         </v-col>
@@ -54,20 +84,26 @@
             </v-container>
             <v-divider class="my-2"></v-divider>
             <v-container class="text-body-1">
-                <p class="pb-6 text-h6"> ค่าลดหย่อนอื่น ๆ </p>
+                <p class="pb-6 text-h6">ค่าลดหย่อนอื่น ๆ</p>
                 <v-row id="loan">
                     <v-col cols="4">
-                        <p> ดอกเบี้ยเงินกู้ยืม <span class="text-red-darken-1"> *</span> </p>
+                        <p>ดอกเบี้ยเงินกู้ยืม <span class="text-red-darken-1"> *</span></p>
                     </v-col>
                     <v-col cols="3">
-                        <v-radio-group v-model="_tax.loan" inline>
+                        <v-radio-group v-model="tax.loan" inline>
                             <v-radio label="ไม่มี" value="N"></v-radio>
                             <v-radio label="มี" value="Y"></v-radio>
                         </v-radio-group>
                     </v-col>
-                    <v-col v-if="_tax.loan == 'Y'">
-                        <v-text-field v-model="_tax.loan_amount" label="จำนวนเงิน (บาท)" hint="โปรดระบุ" density="compact"
-                            variant="outlined" maxLength="10">
+                    <v-col v-if="tax.loan == 'Y'">
+                        <v-text-field
+                            v-model="tax.loan_amount"
+                            label="จำนวนเงิน (บาท)"
+                            hint="โปรดระบุ"
+                            density="compact"
+                            variant="outlined"
+                            maxLength="10"
+                        >
                             <template #append>บาท</template>
                         </v-text-field>
                     </v-col>
@@ -75,17 +111,23 @@
 
                 <v-row id="insurance">
                     <v-col cols="4">
-                        <p> เบี้ยประกันชีวิต (เจ้าตัว) <span class="text-red-darken-1"> *</span> </p>
+                        <p>เบี้ยประกันชีวิต (เจ้าตัว) <span class="text-red-darken-1"> *</span></p>
                     </v-col>
                     <v-col cols="3">
-                        <v-radio-group v-model="_tax.insurance" inline>
+                        <v-radio-group v-model="tax.insurance" inline>
                             <v-radio label="ไม่มี" value="N"></v-radio>
                             <v-radio label="มี" value="Y"></v-radio>
                         </v-radio-group>
                     </v-col>
-                    <v-col v-if="_tax.insurance == 'Y'">
-                        <v-text-field v-model="_tax.insurance_amount" label="จำนวนเงิน (บาท)" hint="โปรดระบุ"
-                            density="compact" variant="outlined" maxLength="10">
+                    <v-col v-if="tax.insurance == 'Y'">
+                        <v-text-field
+                            v-model="tax.insurance_amount"
+                            label="จำนวนเงิน (บาท)"
+                            hint="โปรดระบุ"
+                            density="compact"
+                            variant="outlined"
+                            maxLength="10"
+                        >
                             <template #append>บาท</template>
                         </v-text-field>
                     </v-col>
@@ -93,17 +135,23 @@
 
                 <v-row id="donated">
                     <v-col cols="4">
-                        <p> เงินบริจาค <span class="text-red-darken-1"> *</span> </p>
+                        <p>เงินบริจาค <span class="text-red-darken-1"> *</span></p>
                     </v-col>
                     <v-col cols="3">
-                        <v-radio-group v-model="_tax.donate" inline>
+                        <v-radio-group v-model="tax.donate" inline>
                             <v-radio label="ไม่มี" value="N"></v-radio>
                             <v-radio label="มี" value="Y"></v-radio>
                         </v-radio-group>
                     </v-col>
-                    <v-col v-if="_tax.donate == 'Y'">
-                        <v-text-field v-model="_tax.donate_amount" label="จำนวนเงิน (บาท)" hint="โปรดระบุ" density="compact"
-                            variant="outlined" maxLength="10">
+                    <v-col v-if="tax.donate == 'Y'">
+                        <v-text-field
+                            v-model="tax.donate_amount"
+                            label="จำนวนเงิน (บาท)"
+                            hint="โปรดระบุ"
+                            density="compact"
+                            variant="outlined"
+                            maxLength="10"
+                        >
                             <template #append>บาท</template>
                         </v-text-field>
                     </v-col>
@@ -111,18 +159,23 @@
 
                 <v-row id="fund">
                     <v-col cols="4">
-                        <p> เงินกองทุน <span class="text-red-darken-1"> *</span>
-                        </p>
+                        <p>เงินกองทุน <span class="text-red-darken-1"> *</span></p>
                     </v-col>
                     <v-col cols="3">
-                        <v-radio-group v-model="_tax.fund" inline>
+                        <v-radio-group v-model="tax.fund" inline>
                             <v-radio label="ไม่มี" value="N"></v-radio>
                             <v-radio label="มี" value="Y"></v-radio>
                         </v-radio-group>
                     </v-col>
-                    <v-col v-if="_tax.fund == 'Y'">
-                        <v-text-field v-model="_tax.fund_amount" label="จำนวนเงิน (บาท)" hint="โปรดระบุ" density="compact"
-                            variant="outlined" maxLength="10">
+                    <v-col v-if="tax.fund == 'Y'">
+                        <v-text-field
+                            v-model="tax.fund_amount"
+                            label="จำนวนเงิน (บาท)"
+                            hint="โปรดระบุ"
+                            density="compact"
+                            variant="outlined"
+                            maxLength="10"
+                        >
                             <template #append>บาท</template>
                         </v-text-field>
                     </v-col>
@@ -130,29 +183,35 @@
 
                 <v-row id="marriage">
                     <v-col cols="4">
-                        <p> เงินได้คู่สมรส <span class="text-red-darken-1"> *</span>
-                        </p>
+                        <p>เงินได้คู่สมรส <span class="text-red-darken-1"> *</span></p>
                     </v-col>
                     <v-col cols="6">
-                        <v-radio-group v-model="_tax.marriage_income">
+                        <v-radio-group v-model="tax.marriage_income">
                             <v-radio label="คู่สมรสไม่มีรายได้" value="1"></v-radio>
                             <v-radio label="คู่สมรสมีรายได้ ไม่ลดหย่อนบุตร" value="2"></v-radio>
                             <v-radio label="คู่สมรสมีรายได้ ลดหย่อนบุตร" value="3"></v-radio>
                         </v-radio-group>
                     </v-col>
                 </v-row>
-                <v-row v-if="_tax.marriage_income == '1'">
-                    <v-col cols="4"> เบี้ยประกันชีวิต (คู่สมรส) กรณีไม่มีเงินรายได้ <span class="text-red-darken-1">
-                            *</span></v-col>
+                <v-row v-if="tax.marriage_income == '1'">
+                    <v-col cols="4">
+                        เบี้ยประกันชีวิต (คู่สมรส) กรณีไม่มีเงินรายได้ <span class="text-red-darken-1"> *</span></v-col
+                    >
                     <v-col cols="3">
-                        <v-radio-group v-model="_tax.marriage_insurance" inline>
+                        <v-radio-group v-model="tax.marriage_insurance" inline>
                             <v-radio label="ไม่มี" value="N"></v-radio>
                             <v-radio label="มี" value="Y"></v-radio>
                         </v-radio-group>
                     </v-col>
-                    <v-col v-if="_tax.marriage_insurance == 'Y'">
-                        <v-text-field v-model="_tax.marriage_insurance_amount" label="จำนวนเงิน (บาท)" hint="โปรดระบุ"
-                            density="compact" variant="outlined" maxLength="10">
+                    <v-col v-if="tax.marriage_insurance == 'Y'">
+                        <v-text-field
+                            v-model="tax.marriage_insurance_amount"
+                            label="จำนวนเงิน (บาท)"
+                            hint="โปรดระบุ"
+                            density="compact"
+                            variant="outlined"
+                            maxLength="10"
+                        >
                             <template #append>บาท</template>
                         </v-text-field>
                     </v-col>
@@ -163,13 +222,10 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
-import { usePersonalStore } from '../../stores/personal.store';
-import { ITax } from '~/stores/interface/personal_information.interface';
+import { reactive } from 'vue'
+import { usePersonalStore } from '../../stores/personal.store'
+import { ITax } from '~/stores/interface/personal_information.interface'
 
-const personalStore = usePersonalStore();
-const _tax: ITax = reactive({
-    ...personalStore.$state.tax
-})
-
+const personalStore = usePersonalStore()
+const { tax } = personalStore
 </script>
