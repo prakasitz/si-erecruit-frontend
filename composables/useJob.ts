62f3598d70bea5725b3d545c60471d2d922531f8
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-13 14:52:45 +0700

    create composable for api

---
+++ composables/useJob.ts
@@ -0,0 +1,20 @@
+export default function useJob() {
+    return {
+        loginCandidate,
+        getUserInfo,
+        getStatus
+    };
+  }
+
+async function loginCandidate() {
+
+}
+
+async function getUserInfo() {
+
+}
+
+async function getStatus() {
+
+}
+
---
+++ composables/useProfile.ts
@@ -0,0 +1,20 @@
+export default function useProfile() {
+    return {
+        loginCandidate,
+        getUserInfo,
+        getStatus
+    };
+  }
+
+async function loginCandidate() {
+
+}
+
+async function getUserInfo() {
+
+}
+
+async function getStatus() {
+
+}
+
