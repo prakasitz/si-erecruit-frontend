c1def3214a1bdb1e82469a533457e7022e4fe8b1
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-20 16:30:44 +0700

    test hooks nitro

---
+++ server/plugins/hooks.ts
@@ -0,0 +1,11 @@
+import { getClientCredentials } from '../common/authentication'
+
+export default defineNitroPlugin((nitroApp) => {
+    nitroApp.hooks.hook('render:html', async (html, { event }) => {
+        console.log('render:html')
+    })
+
+    nitroApp.hooks.hook('render:response', (response, { event }) => {
+        console.log('render:response')
+    })
+})
