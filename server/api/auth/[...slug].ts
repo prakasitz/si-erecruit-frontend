c008707630c44aa4edd708fc4e2f8fee9ef62b25
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-20 16:34:19 +0700

    create route for candidate login / manage cred_token

--- server/api/auth/[...slug].ts
+++ server/api/auth/[...slug].ts
@@ -1,63 +1,77 @@
-import { createRouter, defineEventHandler, useBase } from 'h3'
+import { createRouter, defineEventHandler, useBase, H3Error, isError } from 'h3'
+import { getClientCredentials, verifyToken } from '../../common/authentication'
+import { backendService } from '../../common/externalApi'
+import { handleErrorRoute } from '../../common/error'
 
 const router = createRouter()
 
 router.get(
+    '/test',
+    defineEventHandler(async (event) => {
+        const a = await getClientCredentials()
+        return a
+    })
+)
+
+router.get(
     '/userinfo',
     defineEventHandler(async (event) => {
-        const accessToken = getCookie(event, 'access_token') as string
+        const CandidateAccessToken = getCookie(event, 'access_token') as string
         // If no token, user is not authenticated
-        if (!accessToken) {
+        if (!CandidateAccessToken) {
             console.log('Error: No access token provided')
             return createError({
                 statusCode: 400,
                 statusMessage: 'No access token provided',
             })
         }
-
-        return { username: 'Lijinx', name: 'ประกาศิต', surname: 'ช่วยรักษา' }
-    })
-)
-
-router.post(
-    '/login',
-    defineEventHandler(async (event) => {
-        const stateOrError = getCookie(event, 'candidate_state') as string
-        // if (stateOrError != 'check-pid')
-        //     return createError({
-        //         statusCode: 400,
-        //         statusMessage: 'Bad Request',
-        //     })
-
-        setCookie(event, 'candidate_state', 'login', {
-            httpOnly: true,
-            secure: true,
-        })
-
-        setCookie(event, 'access_token', 'Bearer abc', {
-            httpOnly: true,
-            secure: true,
-        })
-
-        console.log(event.node.req.headers)
-        const body = await readBody(event)
-        console.log(event.node.req.url)
-
-        return { body, candidate_state: stateOrError }
+        try {
+            const data = await backendService.CandidateUserInfo(CandidateAccessToken)
+            return data
+        } catch (error: H3Error | any) {
+            return handleErrorRoute(error)
+        }
     })
 )
 
 router.post(
     '/check-pid',
     defineEventHandler(async (event) => {
-        console.log(event.node.req.headers)
-        const body = await readBody(event)
-        setCookie(event, 'candidate_state', 'check-pid', {
-            httpOnly: true,
-            secure: true,
-        })
-        console.log(event.node.req.url)
-        return { body, getRouteRules: getRouteRules(event) }
+        try {
+            const body = await readBody(event)
+            if (body?.pid) {
+                const { pid } = body
+                const data = await backendService.CandidateCheckActive(pid)
+                return { body, data }
+            }
+        } catch (error: H3Error | any) {
+            return handleErrorRoute(error)
+        }
+    })
+)
+
+router.post(
+    '/login',
+    defineEventHandler(async (event) => {
+        try {
+            const body = await readBody(event)
+            let { pid, password, isAuto } = body
+            if (!pid && isAuto) {
+                pid = '1100201370643'
+                password = '908183'
+            }
+            const data = await backendService.CandidateLogin(pid, password)
+            const { access_token } = data
+            setCookie(event, 'access_token', `Bearer ${access_token}`, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'strict',
+            })
+
+            return { body, data }
+        } catch (error: H3Error | any) {
+            return handleErrorRoute(error)
+        }
     })
 )
 
--- server/common/authentication.ts
+++ server/common/authentication.ts
@@ -1,7 +1,63 @@
+import axios, { AxiosError } from 'axios'
 import { H3Event, H3Error } from 'h3'
+import { JwksClient } from 'jwks-rsa'
+import * as qs from 'qs'
+import { JwtPayload, VerifyErrors } from 'jsonwebtoken'
+import { getItemStorage, setItemStorage } from './storage'
+import { verifySignature } from './jwt'
 
-export async function isAuthenticated(event: H3Event): Promise<boolean | H3Error> {
-    // Check client platform
-    let accessToken = null
-    return true
+export async function getClientCredentials() {
+    const { urlOauth2, urlADFS, urlUserInfo, clientID, clientSecret } = useRuntimeConfig()
+    if (!urlOauth2 && !clientID && !clientSecret) return new Error('Can not found config variable.')
+
+    let isValid = false
+
+    const token = await getItemStorage('client_credential_token')
+    if (token) isValid = await verifyToken(token.toString())
+
+    if (isValid) {
+        console.log('using Cache token')
+        return token
+    }
+
+    try {
+        const results = await axios.post(
+            urlOauth2,
+            qs.stringify({
+                client_id: clientID,
+                client_secret: clientSecret,
+                grant_type: 'client_credentials',
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }
+        )
+        const access_token = results.data.access_token
+        if (!access_token) throw new Error(`Cannot find Access token`)
+
+        await setItemStorage({ key: 'client_credential_token', values: access_token.toString() })
+        const token = await getItemStorage('client_credential_token')
+        console.log('using fresh token')
+        return token
+    } catch (error: any) {
+        throw new Error('error:' + error)
+    } finally {
+    }
+}
+
+export async function verifyToken(token: string) {
+    try {
+        const isValid = await new Promise<boolean>((resolve, reject) => {
+            verifySignature(token, (err: VerifyErrors | null, userInfo: JwtPayload | undefined) => {
+                if (err) console.log('error form verifyToken', err)
+                if (err) resolve(false)
+                resolve(true)
+            })
+        })
+        return isValid
+    } catch (error: any) {
+        throw new Error('error:' + error)
+    }
 }
