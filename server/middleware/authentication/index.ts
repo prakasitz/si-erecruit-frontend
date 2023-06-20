ccfbc11b21acc50d17dd5780f070d5ad323f186a
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-20 16:31:53 +0700

    add middleware for authen

--- server/middleware/authentication/index.ts
+++ server/middleware/authentication/index.ts
@@ -1,5 +1,5 @@
 // Log all requests
 export default defineEventHandler(async (event) => {
     if (event.node.req.url?.includes('_nuxt')) return
-    console.log('request: ', event.node.req.method, ' ', event.node.req.url)
+    // console.log('request: ', event.node.req.method, ' ', event.node.req.url)
 })
