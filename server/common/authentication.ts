c07261ef10b93cbd0ff7966a0f45337ec1e4b69d
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-28 21:12:07 +0700

    change variable that following NUXT_ prefix

--- server/common/authentication.ts
+++ server/common/authentication.ts
@@ -4,11 +4,12 @@ import { getItemStorage, setItemStorage } from './storage'
 import { verifyToken } from './jwt'
 
 export async function getClientCredentials() {
-    const { urlOauth2, clientID, clientSecret } = useRuntimeConfig()
-    if (!urlOauth2 && !clientID && !clientSecret) return new Error('Can not found config variable.')
+    const { urlOauth2, clientId, clientSecret } = useRuntimeConfig()
+    if (!urlOauth2 && !clientId && !clientSecret) return new Error('Can not found config variable.')
+
+    console.log({ RuntimeConfig: useRuntimeConfig() })
 
     let isValid = false
-
     const token = await getItemStorage('client_credential_token')
     if (token) isValid = await verifyToken(token.toString())
 
@@ -21,7 +22,7 @@ export async function getClientCredentials() {
         const results = await axios.post(
             urlOauth2,
             qs.stringify({
-                client_id: clientID,
+                client_id: clientId,
                 client_secret: clientSecret,
                 grant_type: 'client_credentials',
             }),
