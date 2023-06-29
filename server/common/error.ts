0185a0fd6e8b2a5b1838bb919413de10f2df635e
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-29 09:48:44 +0700

    update json and throw error handler

--- server/common/error.ts
+++ server/common/error.ts
@@ -2,10 +2,11 @@ import { H3Error } from 'h3'
 
 export function handleErrorRoute(error: any) {
     if (error instanceof H3Error) return error
+    let errMsg = error?.message || error.toString() || ' Unknown error.'
     return createError({
         statusCode: 500,
         statusMessage: 'Internal Server Error',
-        message: 'Ops! Server error occurred. Please try again. 😢, ' + error?.message ?? error.toString(),
+        message: 'Ops! Server error occurred. Please try again 😢.| ' + errMsg,
         stack: '',
     })
 }
