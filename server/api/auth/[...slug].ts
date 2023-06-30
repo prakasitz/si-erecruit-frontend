9c0ab27aab9e539db821fa947bbbf3e167e39c90
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-30 14:27:45 +0700

    from backend routor get userinfo by x-role

--- server/api/auth/[...slug].ts
+++ server/api/auth/[...slug].ts
@@ -1,7 +1,8 @@
 import { createRouter, defineEventHandler, useBase, H3Error, isError } from 'h3'
-import { getClientCredentials,  } from '../../common/authentication'
+import { getClientCredentials } from '../../common/authentication'
 import { backendService } from '../../common/externalApi'
 import { handleErrorRoute } from '../../common/error'
+import { RoleEnum, Roles } from '../../../utils/types'
 
 const router = createRouter()
 
@@ -16,18 +17,36 @@ router.get(
 router.get(
     '/userinfo',
     defineEventHandler(async (event) => {
-        const CandidateAccessToken = getCookie(event, 'access_token') as string
+        const token = getCookie(event, 'access_token') as string
+        let role = getHeader(event, 'x-role') as Roles | undefined
+
+        console.log('/userinfo', {
+            token,
+            role,
+        })
         // If no token, user is not authenticated
-        if (!CandidateAccessToken) {
+
+        if (!token) {
             console.log('Error: No access token provided')
             return createError({
                 statusCode: 400,
                 statusMessage: 'No access token provided',
             })
         }
+
         try {
-            const data = await backendService.CandidateUserInfo(CandidateAccessToken)
-            return data
+            switch (role) {
+                case 'HR':
+                    const data1 = await backendService.HRUserInfo(token)
+                    return data1
+                    break
+                case 'CANDIDATE':
+                    const data2 = await backendService.CandidateUserInfo(token)
+                    return data2
+                    break
+                default:
+                    break
+            }
         } catch (error: H3Error | any) {
             return handleErrorRoute(error)
         }
@@ -53,14 +72,27 @@ router.post(
 router.post(
     '/login',
     defineEventHandler(async (event) => {
+        let role = getRequestHeader(event, 'x-role') as Roles | undefined
         try {
             const body = await readBody(event)
-            let { pid, password, isAuto } = body
-            if (!pid && isAuto) {
-                pid = '1100201370643'
-                password = '908183'
+            let { username, pid, password, isAuto } = body
+            let data: any = undefined
+
+            switch (role) {
+                case 'HR':
+                    data = await backendService.HRLogin(username, password)
+                    break
+                case 'CANDIDATE':
+                    if (!pid && isAuto) {
+                        pid = '1100201370643'
+                        password = '908183'
+                    }
+
+                    data = await backendService.CandidateLogin(pid, password)
+                    break
+                default:
+                    throw new Error(`Unknown role: ${role}`)
             }
-            const data = await backendService.CandidateLogin(pid, password)
             const { access_token } = data
             setCookie(event, 'access_token', `Bearer ${access_token}`, {
                 httpOnly: true,
@@ -68,7 +100,7 @@ router.post(
                 sameSite: 'strict',
             })
 
-            return { body, data }
+            return { statusCode: 200 }
         } catch (error: H3Error | any) {
             return handleErrorRoute(error)
         }
