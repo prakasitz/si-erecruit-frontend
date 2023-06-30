339917a3022bee986c49bc61c4e1ea3cd29909dc
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-30 13:15:29 +0700

    clear some useless

--- server/middleware/authentication/index.ts
+++
@@ -1,5 +0,0 @@
-// Log all requests
-export default defineEventHandler(async (event) => {
-    if (event.node.req.url?.includes('_nuxt')) return
-    // console.log('request: ', event.node.req.method, ' ', event.node.req.url)
-})
