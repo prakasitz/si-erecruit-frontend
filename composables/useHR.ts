a67de8fddc8014a2dae4000f903cc4723dfa245e
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-06-30 13:21:04 +0700

    add compose UseHR

--- composables/useHR.ts
+++ composables/useHR.ts
@@ -1,20 +1,50 @@
+import { useUserStore } from '~/stores/user.store'
+
 export default function useHR() {
     return {
         loginHR,
-        getUserInfo,
-        getStatus
-    };
-  }
-
-async function loginHR() {
-
+        fetchUserInfo,
+        getStatus,
+    }
 }
 
-async function getUserInfo() {
+async function loginHR(username?: string | undefined, password?: string | undefined) {
+    const response = await useApi('/auth/login', {
+        method: 'POST',
+        headers: {
+            'x-role': RoleEnum.HR,
+        },
+        body: {
+            username,
+            password,
+        },
+    })
 
+    if (response.error.value?.data)
+        throw createError({
+            statusCode: response.error.value?.statusCode,
+            statusMessage: response.error.value?.message,
+            message: '[m:login-hr]',
+        })
+
+    return response
 }
 
-async function getStatus() {
+async function fetchUserInfo() {
+    const response = await useApi('/auth/userinfo', {
+        method: 'GET',
+    })
 
+    if (response.error.value?.data)
+        throw createError({
+            statusCode: response.error.value?.statusCode,
+            statusMessage: response.error.value?.message,
+        })
+
+    const userStore = (await useUserStore()) as any
+    userStore.setUserInfo(response.data.value)
+
+    return response
 }
 
+async function getStatus() {}
