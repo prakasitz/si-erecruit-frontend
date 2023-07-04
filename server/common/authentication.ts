8b9bcdb3e6b29fbd15a9434c5b5cbf9c8de53f81
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-07-04 11:20:35 +0700

    add simple authen middleware / add context user

--- server/common/authentication.ts
+++ server/common/authentication.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosError } from 'axios'
 import * as qs from 'qs'
 import { getItemStorage, setItemStorage } from './storage'
-import { verifyToken } from './token'
+import { verifyAccessToken, verifyOauth2Token } from './token'
 import { H3Event, H3Error } from 'h3'
 import { User } from '../../utils/types'
+import { JwtPayload } from 'jsonwebtoken'
 
 export async function getClientCredentials() {
     const { urlOauth2, clientId, clientSecret } = useRuntimeConfig()
@@ -11,7 +12,7 @@ export async function getClientCredentials() {
 
     let isValid = false
     const token = await getItemStorage('client_credential_token')
-    if (token) isValid = await verifyToken(token.toString())
+    if (token) isValid = await verifyOauth2Token(token.toString())
 
     if (isValid) {
         console.log('using Cache token')
@@ -45,137 +46,67 @@ export async function getClientCredentials() {
     }
 }
 
-// export async function isAuthenticated(event: H3Event): Promise<boolean | H3Error> {
-//     let accessToken: string = ''
+export async function isAuthenticated(event: H3Event): Promise<boolean | H3Error> {
+    let accessToken = getCookie(event, 'access_token') as string
 
-//     // If no token, user is not authenticated
-//     if (!accessToken) {
-//         console.log('Error: No access token provided')
-//         return createError({
-//             statusCode: 400,
-//             statusMessage: 'No access token provided',
-//         })
-//     }
+    // // If no token, user is not authenticated
+    if (!accessToken) {
+        console.log('Error: No access token provided')
+        return createError({
+            statusCode: 400,
+            statusMessage: 'No access token provided',
+        })
+    }
 
-//     // Parse Bearer token (Bearer xxxx)
-//     const accessTokenArr = accessToken.split(' ')
+    // Parse Bearer token (Bearer xxxx)
+    const accessTokenArr = accessToken.split(' ')
+    const errorOrUser = await verifyAccessToken(accessTokenArr[1])
+    // // If other error, return error
+    // If other error, return error
+    if (errorOrUser instanceof H3Error) {
+        console.log('Error: isAuthenticated', errorOrUser)
+        return false
+    } else {
+        // Otherwise, we have the user so return true
+        return true
+    }
+}
 
-//     // Verify access token
-//     const errorOrUser = verifyAccessToken(accessTokenArr[1])
+export async function getUserFromAccessToken(event: H3Event): Promise<any> {
+    let accessToken: string = ''
+    let tokenPayload: any = ''
 
-//     // If error is an expired access token, attempt to reauthenticate
-//     if (errorOrUser instanceof jwt.TokenExpiredError) {
-//         console.log('Yes, attempt to reauthenticate')
-//         const errorOrTokens = await getNewTokens(event)
+    accessToken = getCookie(event, 'access_token') as string
 
-//         // If get an error, reauthentication failed
-//         if (errorOrTokens instanceof H3Error) {
-//             console.log('Reauthentication failed')
-//             return createError({
-//                 statusCode: 500,
-//                 statusMessage: 'Reauthentication failed. Login required.',
-//             })
-//         }
+    console.log('getUserFromAccessToken: Attempt to get user from access token')
 
-//         // Otherwise, get tokens
-//         const tokens = errorOrTokens as TokensSession
+    // If no token, display error and return false
+    if (!accessToken) {
+        console.log('getUserFromAccessToken: No access token provided. Cannot get user from access token')
+        return null
+    }
 
-//         const errorOrPlatform = getClientPlatform(event)
-//         if (errorOrPlatform instanceof H3Error) return errorOrPlatform
+    // Verify access token
+    const accessTokenArr = accessToken.split(' ')
+    const errorOrUser = verifyAccessToken(accessTokenArr[1])
 
-//         // Get access token from header or cookie
-//         const platform = errorOrPlatform as string
+    // If error, print to console and return false
+    if (errorOrUser instanceof H3Error) {
+        console.log(errorOrUser)
+        console.log('getUserFromAccessToken: Error verifying access token')
+        return null
+    }
 
-//         // If platform is app dev/production, set tokens in header
-//         if (platform === 'app') {
-//             setHeader(event, 'iam-access-token', 'Bearer ' + tokens.accessToken)
-//             setHeader(event, 'iam-refresh-token', 'Bearer ' + tokens.refreshToken)
-//             if (tokens.sid) setHeader(event, 'iam-sid', tokens.sid)
-//         }
+    // Otherwise, get token payload
+    tokenPayload = errorOrUser as JwtPayload
 
-//         // If platform is browser production, set tokens in secure, httpOnly cookies
-//         if (platform === 'browser') {
-//             setCookie(event, 'iam-access-token', 'Bearer ' + tokens.accessToken, {
-//                 httpOnly: true,
-//                 secure: true,
-//             })
+    // If no user, show error, return false
+    if (tokenPayload === null) {
+        console.log('Failed to get user to check for isSuperAdmin')
+        return null
+    }
 
-//             // Cookies containing refresh tokens expire in 14 days, unless refreshed and new tokens obtained
-//             // Refresh tokens themselves expire in 14 days, unless new tokens are obtained
-//             setCookie(event, 'iam-refresh-token', 'Bearer ' + tokens.refreshToken, {
-//                 httpOnly: true,
-//                 secure: true,
-//                 expires: dayjs().add(14, 'day').toDate(),
-//             })
-
-//             // Set session id
-//             if (tokens.sid) setCookie(event, 'iam-sid', tokens.sid)
-//         }
-
-//         // Development cookies are not secure. Use only in development
-//         if (platform === 'browser-dev') {
-//             setCookie(event, 'iam-access-token', 'Bearer ' + tokens.accessToken, {
-//                 // Access tokens themselves expire in 15 mins
-//                 expires: dayjs().add(1, 'day').toDate(),
-//             })
-//             setCookie(event, 'iam-refresh-token', 'Bearer ' + tokens.refreshToken, {
-//                 expires: dayjs().add(1, 'day').toDate(),
-//             })
-//             // Set session id
-//             if (tokens.sid) setCookie(event, 'iam-sid', tokens.sid)
-//         }
-
-//         // Return authenticated
-//         console.log('Reauthentication successful')
-//         return true
-//     }
-
-//     // If other error, return error
-//     if (errorOrUser instanceof H3Error) {
-//         console.log('Error: ', errorOrUser)
-//         return false
-//     } else {
-//         // Otherwise, we have the user so return true
-//         return true
-//     }
-// }
-
-// export async function getUserFromAccessToken(event: H3Event): Promise<User | null> {
-//     let accessToken: string = ''
-//     let tokenPayload: string = ''
-
-//     console.log('Attempt to get user from access token')
-
-//     // If no token, display error and return false
-//     if (!accessToken) {
-//         console.log('No access token provided. Cannot get user from access token')
-//         return null
-//     }
-
-//     // Verify access token
-//     const accessTokenArr = accessToken.split(' ')
-//     const errorOrToken = verifyAccessToken(accessTokenArr[1])
-
-//     // If error, print to console and return false
-//     if (errorOrToken instanceof H3Error) {
-//         console.log(errorOrToken)
-//         console.log('Error verifying access token')
-//         return null
-//     }
-
-//     // Otherwise, get token payload
-//     tokenPayload = errorOrToken as JwtPayload
-
-//     // Get user by uuid
-//     const userOrNull = await getUserByUuid(tokenPayload.uuid)
-
-//     // If no user, show error, return false
-//     if (userOrNull === null) {
-//         console.log('Failed to get user to check for isSuperAdmin')
-//         return null
-//     }
-
-//     // Otherwise get  and return
-//     const user = userOrNull as User
-//     return user
-// }
+    // // Otherwise get  and return
+    const user = tokenPayload as User
+    return user
+}
---
+++ server/middleware/authentication/index.ts
@@ -0,0 +1,45 @@
+/**
+ * Checks if request url is a url that requires authentication
+ * If user is authenticated, adds user to context
+ */
+
+import { H3Error } from 'h3'
+import { getUserFromAccessToken, isAuthenticated } from '../../common/authentication'
+// import { isAuthenticated } from '../../../common/authentication'
+
+const forbiddenError = createError({
+    statusCode: 403,
+    statusMessage: 'Forbidden',
+})
+
+export default defineEventHandler(async (event) => {
+    // Get all routes that need a user to be authenticated
+    const authRoutes = getAuthenticatedRoutes()
+    if (event.node.req.url) {
+        for (let i = 0; i < authRoutes.length; i++) {
+            if (event.node.req.url.includes(authRoutes[i])) {
+                console.log(`=============`, ' ', `middleware:authentication`, ' ', `==========================`)
+                const authenticated = await isAuthenticated(event)
+
+                if (authenticated instanceof H3Error) throw forbiddenError
+
+                if (authenticated === false) throw forbiddenError
+
+                const userOrNull = await getUserFromAccessToken(event)
+
+                if (userOrNull === null) {
+                    console.log('Missing access token after authentication. This should not happen.')
+                    throw createError({
+                        statusCode: 401,
+                        statusMessage: 'Unauthorized. Missing access token.',
+                    })
+                }
+                console.log(userOrNull)
+                // Add user to context
+                event.context.user = userOrNull
+                console.log(`=======================================================================`)
+                break
+            }
+        }
+    }
+})
