05a32d9617d3093073e3d9209230f0242dd483df
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-07-10 14:15:33 +0700

    create service external master

---
+++ server/common/master.external.ts
@@ -0,0 +1,214 @@
+import { AxiosError } from 'axios'
+import { BackendService, backendService } from './externalApi'
+
+class MasterExternal extends BackendService {
+    private masterSlug: string = 'master-data'
+    constructor() {
+        super()
+    }
+    public async getProvince() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getMaritalStatus() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getBlood() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getTitle(adult: undefined | string) {
+        try {
+            await this.initializeToken()
+            const resp = await this.baseAPI.get(`/${this.masterSlug}/title`, {
+                headers: {
+                    Authorization: 'Bearer ' + this.token,
+                },
+            })
+
+            if (!resp.data) throw new Error(`getTitle: Data not found`)
+
+            let result: any = resp.data
+            if (adult == '1') {
+                result = resp.data.filter((obj: any) => ['1', '2', '3'].includes(obj.form_of_address_key))
+            } else if (adult == '0') {
+                result = resp.data.filter((obj: any) => ['4', '5'].includes(obj.form_of_address_key))
+            }
+
+            return result
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getTitleSpecial() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getTitleAcademic() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getTitleConferred() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getTitleMilitary() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getMilitary() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getTitleTH() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getTitleEN() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getReligion() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getLevel() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getCertificate() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getMajor() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getInstitute() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getRace() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    public async getPosition() {
+        try {
+            await this.initializeToken()
+
+            return this.token
+        } catch (error: AxiosError | any) {
+            throw this.handleError(error)
+        }
+    }
+
+    // public async getMaritalStatus() {
+    //     try {
+    //         await this.initializeToken()
+
+    //         return this.token
+    //     } catch (error: AxiosError | any) {
+    //         throw this.handleError(error)
+    //     }
+    // }
+}
+
+export const masterService = new MasterExternal()
