117c238951a48844c784447faf8eafc3df5624e7
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-07-14 14:45:49 +0700

    add type date time

---
+++ utils/date.ts
@@ -0,0 +1,21 @@
+import dayjs from 'dayjs'
+import 'dayjs/locale/th' // load on demand
+import buddhistEra from 'dayjs/plugin/buddhistEra'
+
+dayjs.extend(buddhistEra)
+dayjs.locale('th')
+
+export enum DateFormatEnum {
+    DATE_TIME_BUDDHIST_1 = 'DD MMM BBBB, HH:mm',
+    DATE_TIME_GENERAL = 'DD/MMM/YYYY HH:mm',
+    DATE_ONLY = 'DD/MM/YYYY',
+    TIME_ONLY_12_HOUR = 'h:mm A',
+    TIME_ONLY_24_HOUR = 'HH:mm',
+    MONTH_YEAR = 'MMMM YYYY',
+    MONTH_DAY_YEAR = 'MMMM DD, YYYY',
+    DAY_MONTH_YEAR = 'DD MMMM YYYY',
+}
+
+export const dateToString = (date: string | Date, format: DateFormatEnum) => {
+    return dayjs(date).format(format)
+}
