f9117705494b948539cf5f1beaa4e4262db02cc1
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-08-15 15:09:14 +0700

    fix education master list

--- components/forms/EducationForm.vue
+++ components/forms/EducationForm.vue
@@ -1,147 +1,153 @@
 <template>
-    <v-row>
-        <v-col cols="10">
-            <span class="text-h5"></span>
-        </v-col>
-        <v-col class="text-right">
-            <v-btn
-                v-if="props.index != 1"
-                @click="emit('update:trash', props.index)"
-                icon="mdi-trash-can"
-                density="compact"
-                color="red"
-                variant="tonal"
-            >
-            </v-btn>
-        </v-col>
-    </v-row>
-    <v-row>
-        <v-col cols="3"> ระดับการศึกษา<span class="text-red-darken-1"> *</span></v-col>
-        <v-col>
-            <v-autocomplete
-                v-model="educationFormModel.education_level"
-                :items="levelData"
-                :itemTitle="'establishment_text'"
-                :itemValue="'establishment'"
-                variant="outlined"
-                density="compact"
-                :rules="rules_fieldEmpty"
-            >
-            </v-autocomplete>
-        </v-col>
-    </v-row>
-    <v-row>
-        <v-col cols="3">คุณวุฒิ/ปริญญา <span class="text-red-darken-1"> *</span> </v-col>
-        <v-col>
-            <v-autocomplete
-                v-model="educationFormModel.degree"
-                :loading="cerPending"
-                :items="cerData"
-                :itemTitle="'cerfificate_text'"
-                :itemValue="'certificate_code'"
-                variant="outlined"
-                density="compact"
-                :rules="rules_fieldEmpty"
-            >
-            </v-autocomplete>
-        </v-col>
-    </v-row>
-    <v-row>
-        <v-col cols="3"> สถานศึกษา <span class="text-red-darken-1"> *</span></v-col>
-        <v-col>
-            <v-autocomplete
-                v-model="educationFormModel.school"
-                :loading="institutePending"
-                :items="instituteData"
-                :itemTitle="'institute'"
-                :itemValue="'institute_code'"
-                variant="outlined"
-                density="compact"
-                :rules="rules_fieldEmpty"
-            >
-            </v-autocomplete>
-        </v-col>
-        <v-col cols="9" offset="3">
-            <v-text-field
-                v-if="educationFormModel.school === '9999999999'"
-                v-model="educationFormModel.school_other"
-                label="อื่นๆ โปรดระบุ"
-                variant="outlined"
-                density="compact"
-            ></v-text-field>
-        </v-col>
-    </v-row>
-    <v-row>
-        <v-col cols="3">สาขาวิชา <span class="text-red-darken-1"> *</span></v-col>
-        <v-col>
-            <v-autocomplete
-                v-model="educationFormModel.major"
-                :loading="majorPending"
-                branch_of_study_code
-                :itemTitle="'branch_of_study_text'"
-                :itemValue="'branch_of_study_code'"
-                :items="majorData"
-                variant="outlined"
-                density="compact"
-                :rules="rules_fieldEmpty"
-            >
-            </v-autocomplete>
-        </v-col>
-        <v-col cols="9" offset="3">
-            <v-text-field
-                v-if="educationFormModel.major === '99997'"
-                v-model="educationFormModel.major_other"
-                label="อื่นๆ โปรดระบุ"
-                variant="outlined"
-                density="compact"
-            ></v-text-field>
-        </v-col>
-    </v-row>
-
-    <v-row>
-        <v-col cols="3"> เกรดเฉลี่ย <span class="text-red-darken-1"> *</span> </v-col>
-        <v-col cols="3">
-            <v-text-field
-                v-model="educationFormModel.gpa"
-                hint="โปรดระบุ"
-                density="compact"
-                variant="outlined"
-                maxLength="4"
-                :rules="rules_fieldEmpty"
-            >
-            </v-text-field
-        ></v-col>
-    </v-row>
-    <v-row>
-        <v-col cols="3"> วันที่เริ่มต้นการศึกษา <span class="text-red-darken-1"> *</span></v-col>
-        <v-col cols="3">
-            <v-input v-model="educationFormModel.start_date" :rules="rules_fieldEmpty">
-                <template #default="{ isValid }">
-                    <VueDatePicker
-                        placeholder="วัน/เดือน/ปี"
-                        :="defaultVueDatePickerStyle"
-                        v-model="educationFormModel.start_date"
-                        :rules="rules_fieldEmpty"
-                        :state="(isValid.value as boolean)"
-                    />
-                </template>
-            </v-input>
-        </v-col>
-        <v-col cols="3"> วันที่จบการศึกษา <span class="text-red-darken-1"> *</span></v-col>
-        <v-col cols="3">
-            <v-input v-model="educationFormModel.graduate_date" :rules="rules_fieldEmpty">
-                <template #default="{ isValid }">
-                    <VueDatePicker
-                        placeholder="วัน/เดือน/ปี"
-                        :="defaultVueDatePickerStyle"
-                        v-model="educationFormModel.graduate_date"
-                        :rules="rules_fieldEmpty"
-                        :state="(isValid.value as boolean)"
-                    />
-                </template>
-            </v-input>
-        </v-col>
-    </v-row>
+    <div>
+        <v-row>
+            <v-col cols="10">
+                <span class="text-h5"></span>
+            </v-col>
+            <v-col class="text-right">
+                <v-btn
+                    v-if="props.index != 1"
+                    @click="emit('update:trash', props.index)"
+                    icon="mdi-trash-can"
+                    density="compact"
+                    color="red"
+                    variant="tonal"
+                >
+                </v-btn>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3"> ระดับการศึกษา<span class="text-red-darken-1"> *</span></v-col>
+            <v-col>
+                <v-autocomplete
+                    v-model="educationFormModel.education_level"
+                    :items="levelData"
+                    :itemTitle="'establishment_text'"
+                    :itemValue="'establishment'"
+                    variant="outlined"
+                    density="compact"
+                    :rules="rules_fieldEmpty"
+                >
+                </v-autocomplete>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3">คุณวุฒิ/ปริญญา <span class="text-red-darken-1"> *</span> </v-col>
+            <v-col>
+                <v-autocomplete
+                    v-model="educationFormModel.degree"
+                    :loading="cerPending"
+                    :items="cerData"
+                    :itemTitle="'cerfificate_text'"
+                    :itemValue="'certificate_code'"
+                    variant="outlined"
+                    density="compact"
+                    :rules="rules_fieldEmpty"
+                    :disabled="cerData.length === 0"
+                >
+                </v-autocomplete>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3"> สถานศึกษา <span v-if="instituteData.length !== 0" class="text-red-darken-1"> *</span></v-col>
+            <v-col>
+                <v-autocomplete
+                    v-model="educationFormModel.school"
+                    :loading="institutePending"
+                    :items="instituteData"
+                    :itemTitle="'institute'"
+                    :itemValue="'institute_code'"
+                    variant="outlined"
+                    density="compact"
+                    :disabled="instituteData.length === 0"
+                    :hide-details="instituteData.length === 0"
+                    :rules="rules_fieldEmpty"
+                >
+                </v-autocomplete>
+            </v-col>
+            <v-col cols="9" offset="3">
+                <v-text-field
+                    v-if="educationFormModel.school === '9999999999'"
+                    v-model="educationFormModel.school_other"
+                    label="อื่นๆ โปรดระบุ"
+                    variant="outlined"
+                    density="compact"
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3">สาขาวิชา <span v-if="majorData.length !== 0" class="text-red-darken-1"> *</span></v-col>
+            <v-col>
+                <v-autocomplete
+                    v-model="educationFormModel.major"
+                    :loading="majorPending"
+                    branch_of_study_code
+                    :itemTitle="'branch_of_study_text'"
+                    :itemValue="'branch_of_study_code'"
+                    :items="majorData"
+                    variant="outlined"
+                    density="compact"
+                    :disabled="majorData.length === 0"
+                    :hide-details="majorData.length === 0"
+                    :rules="rules_fieldEmpty"
+                >
+                </v-autocomplete>
+            </v-col>
+            <v-col cols="9" offset="3">
+                <v-text-field
+                    v-if="educationFormModel.major === '99997'"
+                    v-model="educationFormModel.major_other"
+                    label="อื่นๆ โปรดระบุ"
+                    variant="outlined"
+                    density="compact"
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3"> เกรดเฉลี่ย <span class="text-red-darken-1"> *</span> </v-col>
+            <v-col cols="3">
+                <v-text-field
+                    v-model="educationFormModel.gpa"
+                    hint="โปรดระบุ"
+                    density="compact"
+                    variant="outlined"
+                    maxLength="4"
+                    :rules="rules_fieldEmpty"
+                >
+                </v-text-field
+            ></v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3"> วันที่เริ่มต้นการศึกษา <span class="text-red-darken-1"> *</span></v-col>
+            <v-col cols="3">
+                <v-input v-model="educationFormModel.start_date" :rules="rules_fieldEmpty">
+                    <template #default="{ isValid }">
+                        <VueDatePicker
+                            placeholder="วัน/เดือน/ปี"
+                            :="defaultVueDatePickerStyle"
+                            v-model="educationFormModel.start_date"
+                            :rules="rules_fieldEmpty"
+                            :state="(isValid.value as boolean)"
+                        />
+                    </template>
+                </v-input>
+            </v-col>
+            <v-col cols="3"> วันที่จบการศึกษา <span class="text-red-darken-1"> *</span></v-col>
+            <v-col cols="3">
+                <v-input v-model="educationFormModel.graduate_date" :rules="rules_fieldEmpty">
+                    <template #default="{ isValid }">
+                        <VueDatePicker
+                            placeholder="วัน/เดือน/ปี"
+                            :="defaultVueDatePickerStyle"
+                            v-model="educationFormModel.graduate_date"
+                            :rules="rules_fieldEmpty"
+                            :state="(isValid.value as boolean)"
+                        />
+                    </template>
+                </v-input>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 <script setup lang="ts">
 import { useMasterDataStore } from '~/stores/master.store'
@@ -194,6 +200,8 @@ const { data: instituteData, pending: institutePending } = await useFetch('/api/
     key: 'master/institute' + currentEduLevel.value,
     watch: [currentEduLevel],
 })
+
+const test = ref(true)
 
 watch(currentEduLevel, (old_edu, new_edu) => {
     console.log({
