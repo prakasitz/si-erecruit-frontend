e9cdfa605a01b9160086da87ad59fd1c1fed18c0
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-08-24 15:38:19 +0700

    loading when page on refresh

--- app.vue
+++ app.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
         <NuxtLayout>
-            <NuxtLoadingIndicator />
+            <!-- <NuxtLoadingIndicator /> -->
+            <transition name="fade">
+                <div v-if="loading" class="loading-overlay d-flex justify-center align-center">
+                    <div class="lds-ring">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </transition>
             <v-row class="mb-5">
                 <DialogInfo />
                 <DialogWarning />
@@ -13,6 +23,62 @@
     </div>
 </template>
 <style>
+.loading-overlay {
+    background: linear-gradient(to bottom, #1e88e5, #eef5f9);
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 50%; /* Adjust as needed */
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 10000;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.lds-ring {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+}
+.lds-ring div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: 8px solid #fff;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #fff transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
 /* ... */
 .rotate-enter-active {
     transition: all 0.2s;
@@ -51,4 +117,16 @@
 }
 </style>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const loading = ref(true)
+
+const nuxtApp = useNuxtApp()
+
+nuxtApp.hook('page:finish', () => {
+    if (loading.value == true) {
+        setTimeout(() => {
+            loading.value = false
+        }, 500)
+    }
+})
+</script>
