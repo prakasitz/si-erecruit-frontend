40e3c9c440148311312adcc825b833d4cd9787d1
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-08-25 22:39:25 +0700

    create component loadingPage

--- app.vue
+++ app.vue
@@ -2,16 +2,7 @@
     <div>
         <NuxtLayout>
             <!-- <NuxtLoadingIndicator /> -->
-            <transition name="fade">
-                <div v-if="loading" class="loading-overlay d-flex justify-center align-center">
-                    <div class="lds-ring">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-            </transition>
+            <LoadingPage :loading="loading" />
             <v-row class="mb-5">
                 <DialogInfo />
                 <DialogWarning />
@@ -23,16 +14,7 @@
     </div>
 </template>
 <style>
-.loading-overlay {
-    background: linear-gradient(to bottom, #1e88e5, #eef5f9);
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    top: 50%; /* Adjust as needed */
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 10000;
-}
+
 
 .fade-enter-active,
 .fade-leave-active {
@@ -43,41 +25,7 @@
     opacity: 0;
 }
 
-.lds-ring {
-    display: inline-block;
-    position: relative;
-    width: 80px;
-    height: 80px;
-}
-.lds-ring div {
-    box-sizing: border-box;
-    display: block;
-    position: absolute;
-    width: 64px;
-    height: 64px;
-    margin: 8px;
-    border: 8px solid #fff;
-    border-radius: 50%;
-    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    border-color: #fff transparent transparent transparent;
-}
-.lds-ring div:nth-child(1) {
-    animation-delay: -0.45s;
-}
-.lds-ring div:nth-child(2) {
-    animation-delay: -0.3s;
-}
-.lds-ring div:nth-child(3) {
-    animation-delay: -0.15s;
-}
-@keyframes lds-ring {
-    0% {
-        transform: rotate(0deg);
-    }
-    100% {
-        transform: rotate(360deg);
-    }
-}
+
 
 /* ... */
 .rotate-enter-active {
@@ -118,10 +66,9 @@
 </style>
 
 <script setup lang="ts">
-const loading = ref(true)
+const { loading } = usePageLoading()
 
 const nuxtApp = useNuxtApp()
-
 nuxtApp.hook('page:finish', () => {
     setTimeout(() => {
         if (loading.value == true) {
--- composables/useLoading.ts
+++ composables/useLoading.ts
@@ -1,9 +1,13 @@
 import { useComponentStore } from '~/stores/component.store'
-import { CandidateForm } from '~/utils/types'
 
 export const useLoadingCandidateCard = (id: number) => {
     const componentStore = useComponentStore()
     onMounted(() => {
         componentStore.updateLoadingCandidateForm(id)
     })
+}
+
+export const usePageLoading = () => {
+    const loading = useState<boolean>('loading', () => true)
+    return { loading }
 }
