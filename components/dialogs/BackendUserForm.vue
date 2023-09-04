07b6b6828b9128d8c02cc51c618134a084289eec
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-09-04 22:01:56 +0700

    [update] dialog make password field

--- components/dialogs/BackendUserForm.vue
+++ components/dialogs/BackendUserForm.vue
@@ -2,13 +2,10 @@
     <v-dialog :model-value="props.dialog" width="auto" persistent>
         <v-form ref="userForm">
             <v-card class="mb-5" width="800">
-                <v-toolbar density="compact" :color="'main-color'">
+                <v-toolbar density="compact" :color="bgColor">
                     <v-icon class="ml-2" :icon="'mdi-information-outline'"></v-icon>
                     <v-card-title class="pl-2" :style="{ 'font-size': '16px !important' }">
-                        <div v-if="props.formType == 'edit'">
-                            <span class="text-h6 ml-0"> แก้ไขข้อมูลผู้ใช้ </span>
-                        </div>
-                        <div v-else><span class="text-h6 ml-0"> สร้างข้อมูลผู้ใช้ </span></div>
+                        <span class="text-h6 ml-0"> {{ context.title }} </span>
                     </v-card-title>
                 </v-toolbar>
                 <v-card-text>
@@ -25,7 +22,7 @@
                                 variant="outlined"
                                 counter="8"
                                 :readonly="props.formType == 'edit'"
-                                class="mt-1 sap-id"
+                                class="sap-id"
                             ></v-text-field>
                         </v-col>
                         <v-spacer></v-spacer>
@@ -42,7 +39,6 @@
                                 "
                                 counter="50"
                                 label="SAP Name"
-                                class="mt-1"
                                 variant="outlined"
                                 density="compact"
                             ></v-text-field>
@@ -58,7 +54,6 @@
                                 "
                                 :items="['Super Admin', 'General Admin', 'HR Officer', 'Department Officer']"
                                 label="Role"
-                                class="mt-1"
                                 variant="outlined"
                                 density="compact"
                             >
@@ -69,25 +64,33 @@
                         <v-col cols="5">
                             <v-card variant="outlined">
                                 <v-card-title>User Config</v-card-title>
-                                <v-card-text>
+                                <v-card-text class="pb-0">
                                     <v-row no-gutters>
-                                        <v-col>
-                                            <v-switch v-model="userLock">
+                                        <v-col cols="12">
+                                            <v-switch
+                                                v-model="userLock"
+                                                hide-details
+                                                :color="userLock ? 'main-color' : 'success'"
+                                            >
                                                 <template #label>
                                                     <v-icon
                                                         :icon="userLock ? 'mdi-account-lock' : 'mdi-account-lock-open'"
                                                     >
                                                     </v-icon>
                                                     <div class="ml-2 text-h7 font-weight-bold">
-                                                        {{ userLock ? 'Lock' : 'Unlock' }}
+                                                        {{ userLock ? 'Lock' : 'Active' }}
                                                     </div>
                                                 </template>
                                             </v-switch>
                                         </v-col>
                                     </v-row>
                                     <v-row no-gutters>
-                                        <v-col>
-                                            <v-switch v-model="userLocal">
+                                        <v-col cols="12">
+                                            <v-switch
+                                                v-model="userLocal"
+                                                hide-details
+                                                :color="userLocal ? 'main-color' : ''"
+                                            >
                                                 <template #label>
                                                     <v-icon :icon="userLocal ? 'mdi-account-tie' : 'mdi-account'">
                                                     </v-icon>
@@ -97,6 +100,24 @@
                                                 </template>
                                             </v-switch>
                                         </v-col>
+                                        <v-expand-transition>
+                                            <v-col cols="12" v-if="userLocal">
+                                                <v-text-field
+                                                    v-if="userLocal"
+                                                    :rules="
+                                                        fieldRules({
+                                                            length: 100,
+                                                            type: 'string',
+                                                        })
+                                                    "
+                                                    type="password"
+                                                    counter="12"
+                                                    label="Local-Password"
+                                                    variant="outlined"
+                                                    density="compact"
+                                                ></v-text-field>
+                                            </v-col>
+                                        </v-expand-transition>
                                     </v-row>
                                 </v-card-text>
                             </v-card>
@@ -104,17 +125,29 @@
                         <v-spacer></v-spacer>
                         <v-divider vertical></v-divider>
                         <v-col offset="1" cols="5">
-                            <v-textarea label="Note" counter="255" class="mt-1" variant="outlined" density="compact">
-                            </v-textarea>
+                            <v-textarea label="Note" counter="255" variant="outlined" density="compact"> </v-textarea>
+                            <v-checkbox
+                                hide-details
+                                :rules="fieldRules({})"
+                                density="compact"
+                                v-model="check_accept"
+                                label="ยืนยันการบันทึกข้อมูล"
+                            ></v-checkbox>
                         </v-col>
                     </v-row>
-                    <v-row></v-row>
-                    <v-row></v-row>
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn variant="flat" color="blue" text="Save" @click="emit('update:dialog', false)" />
-                    <v-btn variant="outlined" text="close" @click="emit('update:dialog', false)" />
+                    <v-col cols="3" align-self="center" class="text-right">
+                        <v-btn
+                            variant="flat"
+                            :color="bgColor"
+                            :text="context.btnSubmit"
+                            :disabled="!check_accept"
+                            @click="emit('update:dialog', false)"
+                        />
+                        <v-btn variant="outlined" text="close" @click="emit('update:dialog', false)" />
+                    </v-col>
                 </v-card-actions>
             </v-card>
         </v-form>
@@ -123,14 +156,74 @@
 
 <style scoped>
 .v-input.sap-id:deep() .v-field__input {
-    font-size: 1.5em;
+    font-size: 1.3em;
     font-weight: bold;
 }
 </style>
 
 <script lang="ts" setup>
+import { SRC_User } from '~/utils/types';
+
+/*
+	   [SAP_ID] -- INPUT
+      ,[SAP_name] -- INPUT
+      ,[role_ID] -- INPUT
+      ,[locked_user] -- INPUT CREATE is 0
+      ,[last_login]
+      ,[note] -- INPUT
+	   ,[local_password] -- INPUT
+      ,[local_user] -- INPUT
+      ,[created_at]
+      ,[created_by]
+      ,[name]
+      ,[lastname]
+      */
+
+const currentBgColor = ref('')
+const currentContext = ref(
+    reactive({
+        title: '',
+        btnSubmit: '',
+    })
+)
+
+const context = computed(() => {
+    switch (props.formType) {
+        case 'edit':
+            currentContext.value = {
+                title: 'แก้ไขข้อมูลผู้ใช้',
+                btnSubmit: 'Save',
+            }
+            break
+        case 'create':
+            currentContext.value = {
+                title: 'สร้างข้อมูลผู้ใช้',
+                btnSubmit: 'Create',
+            }
+            break
+        default:
+            break
+    }
+    return currentContext.value
+})
+
+const bgColor = computed(() => {
+    switch (props.formType) {
+        case 'edit':
+            currentBgColor.value = 'amber-darken-4'
+            break
+        case 'create':
+            currentBgColor.value = 'main-color'
+            break
+        default:
+            break
+    }
+    return currentBgColor.value
+})
+
 const { fieldRules } = useFillRules()
 
+const check_accept = ref(false)
 const userLock = ref(false)
 const userLocal = ref(false)
 const rulesConfig = reactive({})
@@ -142,11 +235,14 @@ const props = defineProps({
         type: Boolean,
         required: true,
     },
+    user: {
+        type: Object as PropType<SRC_User>,
+        required: false,
+    },
     formType: {
         type: String as PropType<'create' | 'edit' | ''>,
         required: true,
     },
 })
 
-const dialog = ref(false)
 </script>
