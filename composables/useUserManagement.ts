e0bcff29bbebb60431dec58b3429c68c48c5a9c6
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-09-19 18:40:51 +0700

    add composable for users

--- composables/useUserManagement.ts
+++ composables/useUserManagement.ts
@@ -12,16 +12,19 @@ export default function useUserManagement() {
 const fetchSRCUsers = () => {
     /*
      */
-    return useFetch('/api/external/users', {
+    return useFetch('/api/external/users/get', {
         headers: {
             Accept: 'application/json',
         },
         method: 'GET',
-        transform(data: any) {
+        transform(data: SRC_User[]) {
             return data.map((item: any): SRC_User => {
                 return {
                     SAP_ID: item.SAP_ID,
                     SAP_name: item.SAP_name,
+                    name: item.name,
+                    lastname: item.lastname,
+                    note: item.note,
                     role_ID: item.role_ID,
                     local_user: item.local_user,
                     locked_user: item.locked_user,
@@ -36,12 +39,12 @@ const fetchSRCUsers = () => {
 const fetchSRCUserById = (sap_id: any) => {
     /*
      */
-    return useFetch(`/api/external/users/${sap_id}`, {
+    return useFetch(`/api/external/users/get/${sap_id}`, {
         headers: {
             Accept: 'application/json',
         },
         method: 'GET',
-        transform(data: any) {
+        transform(data: SRC_User) {
             return data
         },
         server: false,
@@ -82,7 +85,7 @@ const updateSRCUserById = (srcUser: SRC_User) => {
 const deleteSRCUserById = (srcUser: SRC_User) => {
     /*
      */
-    return useFetch(`/api/external/users/${srcUser.SAP_ID}`, {
+    return useFetch(`/api/external/users/delete/${srcUser.SAP_ID}`, {
         headers: {
             Accept: 'application/json',
         },
