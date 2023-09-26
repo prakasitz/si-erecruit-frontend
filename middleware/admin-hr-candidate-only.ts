3066a434a3ed50e7d343f5760af9adb8294f80c5
Prakasit Chuayraksa <prakasit.chu@mahidol.ac.th>
2023-09-26 13:41:22 +0700

    add new middleware for check addmin|hr|candidate

--- middleware/admin-hr-candidate-only.ts
+++ middleware/admin-hr-candidate-only.ts
@@ -5,15 +5,24 @@ import { useUserStore } from '~/stores/user.store'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const { setUserInfo } = useUserStore()
-    const { pageLayout } = storeToRefs(useUserStore())
+    const { isHR, isAdmin, isCandidate, pageLayout } = storeToRefs(useUserStore())
     const { me, decryptSecret } = useAuth()
     try {
         const userInfo = await me()
         userInfo.secret = await decryptSecret(userInfo.secret)
         await setUserInfo(userInfo, process.server)
+
         usePageLayout(pageLayout.value)
+
+        if (!isHR.value && !isAdmin.value && !isCandidate.value) {
+            throw createError({
+                statusCode: 403,
+                message: 'You are not authorized to access this page. #T-01-2',
+                stack: undefined,
+            })
+        }
     } catch (error: NuxtError | any) {
-        console.error(error, 'authentication error on auth middleware')
+        console.error(error, 'authentication error on admin-hr-only middleware')
         throw error
     }
 })
